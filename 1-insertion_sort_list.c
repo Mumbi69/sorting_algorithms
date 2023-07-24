b67fcb17a105165a7561e7d5fd25e8e0c285ee11
Mumbi69 <winfriendmumbi69@gmail.com>
2023-07-24 21:01:22 +0000

    Sorting algorithms & Big O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,47 +1,37 @@
 #include "sort.h"
-#include <stdbool.h>
-#include <stdio.h>
-
-/**
-* swap - swaps the list and arrays
-* @m: first integer
-* @n: second integer
-*/
-
-void swap(int *m, int *n)
-{
-	int temp = *m;
-	*m = *n;
-	*n = temp;
-}
 
 /**
 * insertion_sort_list - sorts a doubly linked list using insertion algorithm
 * @list: the list to be sorted
 */
 
-void insertion_sort_list(listint_t **list);
+void insertion_sort_list(listint_t **list)
 {
-	listint_t prev, next;
+	listint_t *current, *next;
 
+	if (list == NULL || (*list)->next == NULL)
+		return;
 
+	current = (*list)->next;
+	while (current)
+	{
+		next = current->next;
+		while (current->prev && (current->prev->n > current->n))
+		{
+			current->prev->next = current->next;
+			if (current->next)
+			current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->prev->prev;
+			current->next->prev = current;
 
+			if (current->prev)
+				current->prev->next = current;
+			else
+				*list = current;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			print_list(*list);
+		}
+		current = next;
+	}
+}
